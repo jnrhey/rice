a9a6f3f9aacb2a95a135ae0ce4518bd945ef1d40
jnrhey <92171893+jnrhey@users.noreply.github.com>
2022-05-10 10:47:21 +0800

    Add files via upload

---
+++ mobi/mobi/MainActivity.cs
@@ -0,0 +1,78 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Firebase;
+using Firebase.Auth;
+
+using mobi.Activities;
+using mobi.Helper;
+using mobi.Listeners;
+using System;
+using ListFragment = mobi.Activities.ListFragment;
+
+namespace mobi
+{
+    [Activity(Label = "MOBIRice", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        UserEventListener userEventListener = new UserEventListener();
+
+        public static BottomNavigationView bottomnavigation;
+       
+
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            try
+            {
+                base.OnCreate(savedInstanceState);
+                SetContentView(Resource.Layout.activity_main);
+                bottomnavigation = (BottomNavigationView)FindViewById(Resource.Id.bottom_navigation);
+                bottomnavigation.NavigationItemSelected += Bottomnavigation_NavigationItemSelected;
+                LoadFragment(Resource.Id.action_list);
+                userEventListener.Create();
+               
+
+                
+
+            } catch (Exception ex) { }
+            
+        }
+
+        private void Bottomnavigation_NavigationItemSelected(object sender, BottomNavigationView.NavigationItemSelectedEventArgs e)
+        {
+            LoadFragment(e.Item.ItemId);
+        }
+
+        public void LoadFragment(int id)
+        {
+            var frag = SupportFragmentManager.BeginTransaction(); 
+            switch (id)
+            {
+                case Resource.Id.action_cart:
+                    CartFragment cartFragment = new CartFragment();
+                    frag.Replace(Resource.Id.landing_layout, cartFragment);
+                    break;
+
+                case Resource.Id.action_list:
+                    ListFragment listFragment = new ListFragment();
+                    frag.Replace(Resource.Id.landing_layout, listFragment);
+                    break;
+
+                case Resource.Id.action_user:
+                    UserFragment userFragment = new UserFragment();
+                    frag.Replace(Resource.Id.landing_layout,userFragment);
+                    break;
+            }
+            frag.Commit();
+        }
+      
+
+    }
+
+}
---
+++ mobi/mobi/Rice.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mobi
+{
+    public class Rice
+    {
+       
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string itemThumbnail { get; set; }
+        public string Price { get; set; }
+        public string ID { get; set; }
+   
+
+
+    }
+}
